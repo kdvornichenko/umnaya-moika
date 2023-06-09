518d1e47feccff390998c3ed65b9e8644df95115
Дворниченко Кирилл (ITS) <gglade999@gmail.com>
2023-06-10 00:55:12 +0300

    свайпер кейсов с адаптацией

--- layout/src/js/components/swiper.js
+++ layout/src/js/components/swiper.js
@@ -1,7 +1,26 @@
-import Swiper from 'swiper';
-import 'swiper/css';
+import Swiper, { Pagination, Navigation } from 'swiper';
+
+// configure Swiper to use modules
+Swiper.use([Pagination, Navigation]);
+import 'swiper/css/bundle';
 
 const swiperCases = new Swiper('.swiper-cases', {
     slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    breakpoints: {
+        1300: {
+            spaceBetween: 0,
+        },
+        320: {
+            spaceBetween: 30,
+        },
+    },
 });
 swiperCases;
